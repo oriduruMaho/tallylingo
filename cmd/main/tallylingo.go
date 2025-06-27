4dabc12b7292b44a0df296bbeb93fcab920445e6
oriduruMaho <oridurumai@gmail.com>
2025-06-27 11:42:32 +0900

    補完部分のオプションを修正した

--- cmd/main/tallylingo.go
+++ cmd/main/tallylingo.go
@@ -38,6 +38,7 @@ type options struct {
 	targets  *CountingTargets
 	printer  *PrintOptions
 	logLevel string
+	generateCompletions bool
 }
 
 func buildFlagSet() (*flag.FlagSet, *options) {
@@ -50,7 +51,7 @@ func buildFlagSet() (*flag.FlagSet, *options) {
 	flags.BoolVarP(&opts.targets.characters, "characters", "c", false, "Count characters")
 	flags.BoolVarP(&opts.targets.bytes, "bytes", "b", false, "Count bytes")
 	flags.BoolVarP(&opts.printer.help, "help", "h", false, "Print this message")
-	flags.BoolVarP(&completions, "generate-completions", "", false, "generate completions")
+	flags.BoolVarP(&opts.generateCompletions, "generate-completions", "", false, "generate completions")
 	flags.MarkHidden("generate-completions")
 	return flags, opts
 }
